faff41ab056ec4b9f716c7fc29f8b212ec3dea2a
Narong <narong@flowaccount.com>
2024-02-22 11:21:24 +0700

    feat(nx-serverless): update serverless-offline config
    ISSUES CLOSED: \

--- libs/nx-serverless/src/builders/offline/offline.impl.ts
+++ libs/nx-serverless/src/builders/offline/offline.impl.ts
@@ -1,80 +1,74 @@
-import { ChildProcess, execSync, fork, spawn } from 'child_process';
-import * as treeKill from 'tree-kill';
-import { runWaitUntilTargets, startBuild } from '../../utils/target.schedulers';
-import { ExecutorContext, logger } from '@nx/devkit';
-import { promisify } from 'util';
-import * as dotEnvJson from 'dotenv-json';
+import { ChildProcess, spawn } from 'child_process'
+import * as treeKill from 'tree-kill'
+import { runWaitUntilTargets } from '../../utils/target.schedulers'
+import { ExecutorContext, logger } from '@nx/devkit'
+import { promisify } from 'util'
+import * as dotEnvJson from 'dotenv-json'
 import {
   InspectType,
   ServerlessExecuteBuilderOptions,
   SimpleBuildEvent,
-} from '../../utils/types';
-import { getSlsCommand } from '../../utils/packagers';
-import * as fs from 'fs';
-import * as path from 'node:path';
+} from '../../utils/types'
+import { getSlsCommand } from '../../utils/packagers'
+import * as fs from 'fs'
+import * as path from 'node:path'
 
 try {
-  require('dotenv').config();
-} catch (e) {}
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('dotenv').config()
+} catch (e) {
+  console.log(`Error:`, e)
+}
 
-let subProcess: ChildProcess = null;
+let subProcess: ChildProcess = null
 export async function* offlineExecutor(
   options: ServerlessExecuteBuilderOptions,
   context: ExecutorContext
 ) {
   console.log(options)
   process.on('SIGTERM', () => {
-    subProcess?.kill();
-    process.exit(128 + 15);
-  });
+    subProcess?.kill()
+    process.exit(128 + 15)
+  })
   process.on('exit', (code) => {
-    process.exit(code);
-  });
+    process.exit(code)
+  })
 
   if (options.skipBuild) {
     if (options.waitUntilTargets && options.waitUntilTargets.length > 0) {
       const results = await runWaitUntilTargets(
         options.waitUntilTargets,
         context
-      );
+      )
       for (const [i, result] of results.entries()) {
         if (!result.success) {
-          console.log('throw');
+          console.log('throw')
           throw new Error(
             `Wait until target failed: ${options.waitUntilTargets[i]}.`
-          );
+          )
         }
       }
     }
   }
 
-  options.watch = true;
-  // for await (const event of startBuild(options, context)) {
-  //   if (!event.success) {
-  //     logger.error('There was an error with the build. See above.');
-  //     logger.info(`${event.outfile} was not restarted.`);
-  //   }
-  //   logger.info(`handleBuildEvent.`);
-  //   await handleBuildEvent(event, options);
-  //   yield event;
-  // }
+  options.watch = true
 
-  await handleBuildEvent({ success: true}, options);
-  yield ({ success: true});
+  await handleBuildEvent({ success: true }, options)
+  yield ({ success: true })
 
   return new Promise<{ success: boolean }>(() => {
-    success: true;
-  });
+    true
+  })
 }
 async function handleBuildEvent(
-  event: { success: boolean},
+  event: { success: boolean },
   options: ServerlessExecuteBuilderOptions
 ) {
   if ((!event.success || options.watch) && subProcess) {
-    await killProcess();
+    await killProcess()
   }
-  logger.info('running process');
-  runProcess(event, options);
+  logger.info('running process')
+  runProcess(event, options)
 }
 
 function runProcess(
@@ -83,98 +77,95 @@ function runProcess(
 ) {
 
   if (subProcess || !event.success) {
-    return;
+    return
   }
   dotEnvJson({
-    path: `${options.package}/${options.processEnvFile ?? 'env-staging.json'}`
-  });
-  // options.config = `${process.cwd()}/dist/apps/api/lambda.crm/serverless.yml`
-  const slsCommand = getSlsCommand();
-  let stringifiedArgs = `offline --config ${options.config} --stage ${options.stage}`;
-  const args: string[] = [];
-  // args.push('sls');
-  args.push('offline');
-  // args.push('--help');
-  // args.push(`--config=${options.config}`);
-  args.push(`--stage=${options.stage}`);
+    path: `${options.location}/${options.processEnvFile ?? 'env-staging.json'}`
+  })
+  const slsCommand = getSlsCommand()
+  let stringifiedArgs = `offline --config ${options.config} --stage ${options.stage}`
+  const args: string[] = []
+  args.push('offline')
+  args.push(`--stage=${options.stage}`)
 
   const configPath = path.parse(options.config)
-  fs.copyFileSync(options.config, path.join(options.location, configPath.base))
+  fs.copyFileSync(options.config, path.join(options.package, configPath.base))
 
-  if(options.verbose) {
-    stringifiedArgs += ' --verbose';
-    args.push('--verbose');
+  if (options.verbose) {
+    stringifiedArgs += ' --verbose'
+    args.push('--verbose')
   }
-  const fullCommand = `${slsCommand} ${stringifiedArgs}`.trim();
-  console.log(`Executing Command: ${fullCommand} in cwd: ${options.location} `); //${options.package}
-    subProcess = spawn(
-      'npx',
-      ['sls', ...args], {stdio: 'inherit', cwd: options.location}
-    )
+  const fullCommand = `${slsCommand} ${stringifiedArgs}`.trim()
+  console.log(`Executing Command: ${fullCommand} in cwd: ${options.package} `) 
+  subProcess = spawn(
+    'npx',
+    ['sls', ...args], { stdio: 'inherit', cwd: options.package }
+  )
 
-    subProcess.on('message', (message) => {
-      console.log('Message from child process:', message);
-    });
-    subProcess.on('error', (err) => {
-      console.log(err);
-    });
-    subProcess.once('exit', (code) => {
-      if (code === 0) Promise.resolve({ success: true });
-      // If process is killed due to current task being killed, then resolve with success.
-      else Promise.resolve({ success: true });
-    });
+  subProcess.on('message', (message) => {
+    console.log('Message from child process:', message)
+  })
+  subProcess.on('error', (err) => {
+    console.log(err)
+  })
+  subProcess.once('exit', (code) => {
+    if (code === 0) Promise.resolve({ success: true });
+    // If process is killed due to current task being killed, then resolve with success.
+    else Promise.resolve({ success: true })
+  })
 }
 
 async function killProcess() {
   if (!subProcess) {
-    return;
+    return
   }
 
   const promisifiedTreeKill: (pid: number, signal: string) => Promise<void> =
-    promisify(treeKill);
+    promisify(treeKill)
   try {
-    await promisifiedTreeKill(subProcess.pid, 'SIGTERM');
+    await promisifiedTreeKill(subProcess.pid, 'SIGTERM')
   } catch (err) {
     if (Array.isArray(err) && err[0] && err[2]) {
-      const errorMessage = err[2];
-      logger.error(errorMessage);
+      const errorMessage = err[2]
+      logger.error(errorMessage)
     } else if (err.message) {
-      logger.error(err.message);
+      logger.error(err.message)
     }
   } finally {
-    subProcess = null;
+    subProcess = null
   }
 }
 
 function getServerlessArg(options: ServerlessExecuteBuilderOptions) {
-  const args = ['offline', ...options.args];
+  const args = ['offline', ...options.args]
   if (options.inspect === true) {
-    options.inspect = InspectType.Inspect;
+    options.inspect = InspectType.Inspect
   }
   if (options.inspect) {
-    args.push(`--${options.inspect}=${options.host}:${options.port}`);
+    args.push(`--${options.inspect}=${options.host}:${options.port}`)
   }
-  return args;
+  return args
 }
 
 function getExecArgv(options: ServerlessExecuteBuilderOptions) {
-  const args = [];
+  const args = []
   if (options.inspect === true) {
-    options.inspect = InspectType.Inspect;
+    options.inspect = InspectType.Inspect
   }
 
   if (options.inspect) {
-    args.push(`--${options.inspect}=${options.host}:${options.port}`);
+    args.push(`--${options.inspect}=${options.host}:${options.port}`)
   }
-  args.push('offline');
+  args.push('offline')
   for (const key in options) {
-    if (options.hasOwnProperty(key)) {
+    const hasOwnProperty = Object.prototype.hasOwnProperty.call(options, key)
+    if (hasOwnProperty) {
       if (options[key] !== undefined) {
-        args.push(`--${key}=${options[key]}`);
+        args.push(`--${key}=${options[key]}`)
       }
     }
   }
-  return args;
+  return args
 }
 
-export default offlineExecutor;
+export default offlineExecutor
