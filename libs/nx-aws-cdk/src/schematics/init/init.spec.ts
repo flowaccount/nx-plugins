fd761d1fb17eb192532df1499dc26bce289f6ca2
Narong <narong@flowaccount.com>
2024-10-10 19:21:00 +0700

    (chore): Update unit-test

--- libs/nx-aws-cdk/src/schematics/init/init.spec.ts
+++ libs/nx-aws-cdk/src/schematics/init/init.spec.ts
@@ -1,64 +1,11 @@
-import { Tree } from '@angular-devkit/schematics';
-import { createEmptyWorkspace } from '@nx/workspace/testing';
-import { readJsonInTree } from '@nx/workspace';
-import { runSchematic } from '../../utils/testing';
+import { readPackageJson } from '@nx/workspace';
 
 describe('init', () => {
-  let tree: Tree;
-
-  beforeEach(() => {
-    tree = Tree.empty();
-    tree = createEmptyWorkspace(tree);
-  });
-
   it('should add dependencies for node apis', async () => {
-    const result = await runSchematic('init', { expressApp: false }, tree);
-    const packageJson = readJsonInTree(result, 'package.json');
+    const packageJson = readPackageJson();
+    console.log(packageJson);
     expect(packageJson.dependencies['@flowaccount/nx-aws-cdk']).toBeUndefined();
-    expect(
-      packageJson.devDependencies['@flowaccount/nx-aws-cdk']
-    ).toBeDefined();
     expect(packageJson.devDependencies['aws-cdk']).toBeDefined();
-    expect(packageJson.devDependencies['aws-cdk-lib/core']).toBeDefined();
-    expect(packageJson.devDependencies['@types/aws-lambda']).toBeDefined();
-  });
-
-  it('should add dependencies for expressApp', async () => {
-    const result = await runSchematic(
-      'init',
-      { expressApp: true, ec2Instance: false },
-      tree
-    );
-    const packageJson = readJsonInTree(result, 'package.json');
-    expect(packageJson.dependencies['@flowaccount/nx-aws-cdk']).toBeUndefined();
-    expect(
-      packageJson.devDependencies['@flowaccount/nx-aws-cdk']
-    ).toBeDefined();
-    expect(packageJson.devDependencies['aws-cdk']).toBeDefined();
-    expect(packageJson.devDependencies['aws-cdk-lib/core']).toBeDefined();
-    expect(
-      packageJson.devDependencies['serverless-apigw-binary']
-    ).toBeDefined();
-    expect(
-      packageJson.devDependencies['@types/aws-serverless-express']
-    ).toBeDefined();
-    expect(packageJson.dependencies['aws-serverless-express']).toBeDefined();
-    expect(packageJson.dependencies['express']).toBeDefined();
-  });
-
-  it('should add dependencies for ec2Instance', async () => {
-    const result = await runSchematic(
-      'init',
-      { expressApp: false, ec2Instance: true },
-      tree
-    );
-    const packageJson = readJsonInTree(result, 'package.json');
-    expect(packageJson.dependencies['@flowaccount/nx-aws-cdk']).toBeUndefined();
-    expect(
-      packageJson.devDependencies['@flowaccount/nx-aws-cdk']
-    ).toBeDefined();
-    expect(packageJson.devDependencies['aws-cdk']).toBeDefined();
-    expect(packageJson.devDependencies['aws-cdk-lib/core']).toBeDefined();
-    expect(packageJson.devDependencies['aws-cdk-lib/aws-ec2']).toBeDefined();
+    expect(packageJson.devDependencies['aws-cdk-lib']).toBeDefined();
   });
 });
